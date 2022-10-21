6ee781d72f402305339d82a748d6b9368ebe8f35
hiteshjamod <68099447+hiteshjamod@users.noreply.github.com>
2022-10-21 14:38:09 +0530

    Add project files.

---
+++ Practical/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Practical.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace Practical.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+
+        public IActionResult Index()
+        {
+            string url = "https://gorest.co.in/public/v2/users";
+
+            HttpClient client = new HttpClient();
+            var response = client.GetAsync(url).Result;
+
+            string result = response.Content.ReadAsStringAsync().Result;
+
+            List<gorest> data = JsonSerializer.Deserialize<List<gorest>>(result);
+
+            //ViewBag.dataresponse = data;
+            
+            return View(data);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Practical/Models/gorest.cs
@@ -0,0 +1,12 @@
+﻿namespace Practical.Models
+{
+    public class gorest
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string status { get; set; }
+
+    }
+}
